fa98194fad18ad9e811f0fa7dde7fad9e370b301
Anindita Padhi <aninditapadhi7@gmail.com>
2026-06-27 13:53:57 +0530

    routes changes

--- next.config.ts
+++ next.config.ts
@@ -6,8 +6,26 @@ const nextConfig: NextConfig = {
   images: {
     // FIXME(prod): comment or false this in prod, used to fetch images hosted on localhost during dev 
     // dangerouslyAllowLocalIP: true,
-    domains: ["th.bing.com", "www.iitk.ac.in", "localhost", "shantsagar"],
+    //domains: ["th.bing.com", "www.iitk.ac.in", "localhost", "shantsagar"],
+    remotePatterns: [
+    { hostname: "th.bing.com" },
+    { hostname: "www.iitk.ac.in" },
+    { hostname: "localhost" },
+    { hostname: "shantsagar" },
+  ],
   },
+
+  async rewrites(){
+
+    return [
+      {
+        source: "/students/:path*",
+        destination: "http://localhost:3000/students/:path*",
+      }
+
+
+    ]
+  }
   
 };
 
